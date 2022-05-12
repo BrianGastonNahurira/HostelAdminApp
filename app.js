816556c11817dec4d755dc08365a060df32dd8ba
XamuelUG <samweko@gmail.com>
2022-05-12 16:09:27 +0300

    added fetch

--- app.js
+++ app.js
@@ -1,41 +1,43 @@
-const express = require('express');
-const res = require('express/lib/response');
+const express = require("express");
+const res = require("express/lib/response");
 const app = express();
 
-const { engine } = require ('express-handlebars');
+const { engine } = require("express-handlebars");
 
-const port = 505;
+const port = process.env.PORT || 5005;
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.use('/images',express.static(__dirname + 'public/images'));
-app.use('/styling',express.static(__dirname + 'public/styling'));
-app.use('/css',express.static(__dirname + 'public/css'));
-app.use('/js',express.static(__dirname + 'public/js'));
-app.use('/script',express.static(__dirname + 'public/script'));
+app.use("/images", express.static(__dirname + "public/images"));
+app.use("/styling", express.static(__dirname + "public/styling"));
+app.use("/css", express.static(__dirname + "public/css"));
+app.use("/js", express.static(__dirname + "public/js"));
+app.use("/script", express.static(__dirname + "public/script"));
 
+app.set("views", "./views");
+app.set("view engine", "ejs");
 
-app.set('views','./views');
-app.set('view engine', 'ejs')
-
-app.engine('ejs', engine({
+app.engine(
+  "ejs",
+  engine({
     layoutsDir: `${__dirname}/views/layouts`,
-    extname: 'ejs',
-    defaultLayout: 'index',
-    partialsDir:`${__dirname}/views/partials`
-}));
+    extname: "ejs",
+    defaultLayout: "index",
+    partialsDir: `${__dirname}/views/partials`,
+  })
+);
 
-app.get('/',(req,res)=>{
-    res.render('main',{layout:'index'});
-})
-app.get('/addHostel',(req,res)=>{
-    res.render('addHostel',{layout:'hostel'});
-})
+app.get("/", (req, res) => {
+  res.render("main", { layout: "index" });
+});
+app.get("/addHostel", (req, res) => {
+  res.render("addHostel", { layout: "hostel" });
+});
 
-app.all('*',(req,res)=>{
-    res.status(404).send('oooppss!!!!  There is nothing here');
-})
+app.all("*", (req, res) => {
+  res.status(404).send("oooppss!!!!  There is nothing here");
+});
 
-app.listen(port,(req,res)=>{
-    console.log(`the server is listening on port ${port} .........`);
-})
+app.listen(port, (req, res) => {
+  console.log(`the server is listening on port ${port} .........`);
+});
