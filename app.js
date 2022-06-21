e26404a8a80185a38017992850b4c118c5e61556
Gaston256 <nahurirabriangaston@gmail.com>
2022-06-21 22:32:12 +0300

    g

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const express = require("express");
 const res = require("express/lib/response");
 const app = express();
+const axios = require('axios');
 
 const { engine } = require("express-handlebars");
 const { json } = require("express/lib/response");
 
 const port = process.env.PORT || 5002;
+
+// fetch("http://localhost:5001")
+//   .then((response) => {
+//     // Do something with response
+//     console.log('fetch succeessfull......');
+//   })
+//   .catch(function (err) {
+//     console.log("Unable to fetch -", err);
+//   });
+
+  // fetch('http://localhost:5001')
+  // .then(response => response.json())
+  // .then(json => console.log(json))
+
 
 app.use(express.static("public"));
 
@@ -31,6 +46,7 @@ app.engine(
 app.get("/", (req, res) => {
   res.render("main", { layout: "index" });
 });
+
 app.get("/addHostel", (req, res) => {
   res.render("addHostel", { layout: "hostel" });
 });
@@ -38,6 +54,16 @@ app.get("/addHostel", (req, res) => {
 app.get("/addedHostel", (req, res) => {
   res.render("addedHostel", { layout: "addedHostels" });
 });
+
+app.get("/login", (req, res) => {
+  res.render("login", { layout: "login" });
+});
+
+app.get("/register", (req, res) => {
+  res.render("register", { layout: "register" });
+});
+
+
 
 app.all("*", (req, res) => {
   res.status(404).send("oooppss!!!!  There is nothing here");
